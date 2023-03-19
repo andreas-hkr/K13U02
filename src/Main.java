9fa4d9874a2f71fd1eb2e6231eb6618bb9e2cf20
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 10:13:46 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass som kan representerar en position  (latitud / longitud)
+        // Latitud  = grader, minuter, sekunder och nord/syd-riktning
+        // Longitud = grader, minuter, sekunder och öst/väst-riktning
+        //
+        // Landvetter = latitud = 57°39'47" N    longitud = 12°16'58" Ö
+
+
+    }
+}
